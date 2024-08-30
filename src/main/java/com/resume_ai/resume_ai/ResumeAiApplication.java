206c17bb029537853664287d3503e3fe6b6c5775
minhaz <minhazali1938@gmail.com>
2024-08-29 20:22:20 -0400

    initial commit

---
+++ src/main/java/com/resume_ai/resume_ai/ResumeAiApplication.java
@@ -0,0 +1,13 @@
+package com.resume_ai.resume_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAiApplication.class, args);
+	}
+
+}
