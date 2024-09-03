30125d3155ca83e87a1ccf14827481a8c57a0c1c
minhaz <minhazali1938@gmail.com>
2024-09-03 10:44:52 -0400

    init controller

---
+++ src/main/java/com/resume_ai/resume_ai/Controller/RestController.java
@@ -0,0 +1,10 @@
+package com.resume_ai.resume_ai.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/resumeAi")
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+
+}
