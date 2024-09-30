6d7a077ff423ad610d54a68628a0b29b402f854b
minhaz <minhazali1938@gmail.com>
2024-09-30 14:58:51 -0400

    Adding entity fields

--- src/main/java/com/resume_ai/resume_ai/Repository/ResumeAiRepository.java
+++ src/main/java/com/resume_ai/resume_ai/Repository/ResumeAiRepository.java
@@ -24,12 +24,18 @@ public class ResumeAiRepository {
         saveResumeAiEntity.setScore(resumeAiDTO.getTotalScore());
         saveResumeAiEntity.setEval(resumeAiDTO.getEval());
         saveResumeAiEntity.setTimestamp(resumeAiDTO.getTimestamp());
+
         saveResumeAiEntity.setInconsistencies(resumeAiDTO.getInconsistencies());
         saveResumeAiEntity.setExaggerated_claims(resumeAiDTO.getExaggerated_claims());
         saveResumeAiEntity.setLack_of_detail(resumeAiDTO.getLack_of_detail());
         saveResumeAiEntity.setFrequent_job_changes(resumeAiDTO.getFrequent_job_changes());
         saveResumeAiEntity.setMissing_information(resumeAiDTO.getMissing_information());
 
+        saveResumeAiEntity.setInconsistencies_Score(resumeAiDTO.getInconsistencies_Score());
+        saveResumeAiEntity.setExaggerated_claims_Score(resumeAiDTO.getExaggerated_claims_Score());
+        saveResumeAiEntity.setLack_of_detail_Score(resumeAiDTO.getLack_of_detail_Score());
+        saveResumeAiEntity.setFrequent_job_changes_Score(resumeAiDTO.getFrequent_job_changes_Score());
+        saveResumeAiEntity.setMissing_information_Score(resumeAiDTO.getMissing_information_Score());
 
         dynamoDBMapper.save(saveResumeAiEntity);
 
@@ -45,11 +51,18 @@ public class ResumeAiRepository {
             getOneResume.setTotalScore(getResumeEntity.getScore());
             getOneResume.setEval(getResumeEntity.getEval());
             getOneResume.setTimestamp(getResumeEntity.getTimestamp());
+
             getOneResume.setInconsistencies(getOneResume.getInconsistencies());
             getOneResume.setExaggerated_claims(getOneResume.getExaggerated_claims());
             getOneResume.setLack_of_detail(getOneResume.getLack_of_detail());
             getOneResume.setFrequent_job_changes(getOneResume.getFrequent_job_changes());
             getOneResume.setMissing_information(getResumeEntity.getMissing_information());
+
+            getOneResume.setInconsistencies_Score(getOneResume.getInconsistencies_Score());
+            getOneResume.setExaggerated_claims_Score(getOneResume.getExaggerated_claims_Score());
+            getOneResume.setLack_of_detail_Score(getOneResume.getLack_of_detail_Score());
+            getOneResume.setFrequent_job_changes_Score(getOneResume.getFrequent_job_changes_Score());
+            getOneResume.setMissing_information_Score(getOneResume.getMissing_information_Score());
 
         }catch (NullPointerException ex){
             System.out.println(ex);
@@ -65,11 +78,18 @@ public class ResumeAiRepository {
         updatedResumeEntity.setScore(resumeAiDTO.getTotalScore());
         updatedResumeEntity.setEval(resumeAiDTO.getEval());
         updatedResumeEntity.setTimestamp(Timestamp.from(Instant.now()));
+
         updatedResumeEntity.setInconsistencies(resumeAiDTO.getInconsistencies());
         updatedResumeEntity.setExaggerated_claims(resumeAiDTO.getExaggerated_claims());
         updatedResumeEntity.setLack_of_detail(resumeAiDTO.getLack_of_detail());
         updatedResumeEntity.setFrequent_job_changes(resumeAiDTO.getFrequent_job_changes());
         updatedResumeEntity.setMissing_information(resumeAiDTO.getMissing_information());
+
+        updatedResumeEntity.setInconsistencies_Score(resumeAiDTO.getInconsistencies_Score());
+        updatedResumeEntity.setExaggerated_claims_Score(resumeAiDTO.getExaggerated_claims_Score());
+        updatedResumeEntity.setLack_of_detail_Score(resumeAiDTO.getLack_of_detail_Score());
+        updatedResumeEntity.setFrequent_job_changes_Score(resumeAiDTO.getFrequent_job_changes_Score());
+        updatedResumeEntity.setMissing_information_Score(resumeAiDTO.getMissing_information_Score());
 
         DynamoDBMapperConfig dynamoDBMapperConfig = new DynamoDBMapperConfig.Builder()
                 .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.UPDATE)
